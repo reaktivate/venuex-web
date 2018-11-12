4d50a60f052e1d552d48813d3d344b2f9d0b7e2d
Timur Isaiev <timur.s.isaev@gmail.com>
2018-11-12 14:50:19 +0200

    Story Book added
    (cherry picked from commit ece6f5dc8bc339a66b626f18d5ed572fbf8c5f0e)

---
+++ src/stories/elements/AddButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import AddButton from '../../ui/elements/AddButton';
+
+storiesOf('elements/AddButton', module)
+  .add('default', () => <AddButton />);
